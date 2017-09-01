503c2906350305cd769bb6322cc4f88383898af4
Gustavo Rodrigues <gurodrigues1300@gmail.com>
2017-09-01 11:24:24 -0300

    Add Select Level Screen

---
+++ Assets/Scripts/UI/LoadLevels.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LoadLevels : MonoBehaviour {
+
+    //BOTOES
+
+    //1
+    public Button Level1
+    {
+        get { return level1; }
+        set { level1 = value; }
+    }
+    [SerializeField]
+    private Button level1;
+    
+    //2
+    public Button Level2
+    {
+        get { return level2; }
+        set { level2 = value; }
+    }
+    [SerializeField]
+    private Button level2;
+
+
+
+    //FASES
+   
+    public void L1Press()
+    {
+        SceneManager.LoadScene("DEMO 1");
+    }
+
+    public void L2Press()
+    {
+        SceneManager.LoadScene("DEMO 2");
+    }
+
+
+}
--- Assets/Scripts/UI/MoveMapTouch.cs
+++ Assets/Scripts/UI/MoveMapTouch.cs
@@ -2,40 +2,51 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MoveMapTouch : MonoBehaviour {
+public sealed class MoveMapTouch : MonoBehaviour {
 
-    public float speed;
+    [SerializeField]
+    private float speed;
     private Vector3 limiteRight = new Vector3(-1,0,0);
     private Vector3 limiteLeft = new Vector3(-19,0,0);
 
-    // Use this for initialization
-    void Start()
+   
+    private void Start()
+    {
+        LoadResources();  
+    }
+
+    private void LoadResources()
     {
         speed = 0.01f;
     }
 
-   
-    void Update()
+
+    private void Update()
     {
+        MoveMap();
+    }
 
-       
 
+    private void MoveMap()
+    {
         if (transform.position.x > 0f)
         {
             transform.position = Vector3.Lerp(transform.position, limiteRight, Time.deltaTime);
-      
-        } else if (transform.position.x < -20)
+
+        }
+        else if (transform.position.x < -20)
         {
             transform.position = Vector3.Lerp(transform.position, limiteLeft, Time.deltaTime);
 
-        } else
+        }
+        else
         {
             if (Input.touchCount == 1)
             {
                 transform.Translate(Input.touches[0].deltaPosition.x * speed, 0, 0);
             }
         }
-
-
     }
+
+
 }
